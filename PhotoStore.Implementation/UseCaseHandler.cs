b389a0ec5ac036ac8d990f492fc81677e259c4e4
vanja-veapi <vanja.veapi@gmail.com>
2022-08-30 20:14:28 +0200

    Added implementation

---
+++ PhotoStore.Implementation/UseCaseHandler.cs
@@ -0,0 +1,100 @@
+﻿using PhotoStore.Application;
+using PhotoStore.Application.Exceptions;
+using PhotoStore.Application.Logging;
+using PhotoStore.Application.UseCases;
+using PhotoStore.Domain;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoStore.Implementation
+{
+    public class UseCaseHandler
+    {
+        private IExceptionLogger _logger;
+        private IApplicationUser _user;
+        private IUseCaseLogger _useCaseLogger;
+
+        public UseCaseHandler(
+            IExceptionLogger logger,
+            IApplicationUser user,
+            IUseCaseLogger useCaseLogger)
+        {
+            _logger = logger;
+            _user = user;
+            _useCaseLogger = useCaseLogger;
+        }
+
+        public void HandleCommand<TRequest>(ICommand<TRequest> command, TRequest data)
+        {
+            try
+            {
+                HandleLoggingAndAuthorization(command, data);
+
+                var stopwatch = new Stopwatch();
+                stopwatch.Start();
+
+                command.Execute(data);
+
+                stopwatch.Stop();
+
+                Console.WriteLine(command.Name + " Duration: " + stopwatch.ElapsedMilliseconds + " ms.");
+            }
+            catch (Exception ex)
+            {
+                _logger.Log(ex);
+                throw;
+            }
+        }
+
+        public TResponse HandleQuery<TRequest, TResponse>(IQuery<TRequest, TResponse> query, TRequest data)
+        {
+            try
+            {
+                HandleLoggingAndAuthorization(query, data);
+
+                var stopwatch = new Stopwatch();
+                stopwatch.Start();
+
+                var response = query.Execute(data);
+
+                stopwatch.Stop();
+
+                Console.WriteLine(query.Name + " Duration: " + stopwatch.ElapsedMilliseconds + " ms.");
+
+                return response;
+            }
+            catch (Exception ex)
+            {
+                _logger.Log(ex);
+                throw;
+            }
+        }
+
+        private void HandleLoggingAndAuthorization<TRequest>(IUseCase useCase, TRequest data)
+        {
+            var isAuthorized = _user.UseCaseIds.Contains(useCase.Id);
+
+            var log = new UseCaseLog
+            {
+                User = _user.Identity,
+                ExecutionDateTime = DateTime.UtcNow,
+                UseCaseName = useCase.Name,
+                UserId = _user.Id,
+                Data = JsonConvert.SerializeObject(data),
+                IsAuthorized = isAuthorized
+            };
+
+            _useCaseLogger.Log(log);
+
+            if (!isAuthorized)
+            {
+                throw new ForbiddenUseCaseExecutionException(useCase.Name, _user.Identity);
+            }
+        }
+    }
+}
